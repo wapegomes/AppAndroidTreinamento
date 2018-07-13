bdaf8e6943eb827de2e7d7a3a407ecc7150368e9
Wagner Pereira Gomes <wgomes@maccit019822.local>
2018-07-13 15:10:37 -0300

    API comnsumo events

--- app/src/main/java/br/com/wpgomes/treinamento/appcasttreinamento/ui/fragments/EventFragment.java
+++ app/src/main/java/br/com/wpgomes/treinamento/appcasttreinamento/ui/fragments/EventFragment.java
@@ -9,8 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import br.com.wpgomes.treinamento.appcasttreinamento.Mock;
+import br.com.wpgomes.treinamento.appcasttreinamento.api.EventApi;
+import br.com.wpgomes.treinamento.appcasttreinamento.model.Event;
 import br.com.wpgomes.treinamento.appcasttreinamento.ui.adapters.EventAdapter;
 import br.com.wpgomes.treinamento.appcasttreinamento.R;
 
@@ -49,9 +54,36 @@ public class EventFragment extends Fragment {
         }
 
 
-//        eventAdapter = new EventAdapter(getActivity(), Mock.getEvents(),
-//                recyclerView, isTablet, webView);
-//        recyclerView.setAdapter(eventAdapter);
+        getEvents(isTablet);
+    }
+
+
+    private void getEvents(final Boolean isTablet) {
+
+        final EventApi eventApi = new EventApi(getActivity());
+        eventApi.events(new EventApi.OnEventListener() {
+            @Override
+            public void onEventListener(final List<Event> events, int errorCode) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (events != null) {
+                            if (isTablet) {
+                                eventAdapter = new EventAdapter(getActivity(), events, recyclerView,
+                                        isTablet, webView);
+                            } else {
+                                eventAdapter = new EventAdapter(getActivity(), events, recyclerView,
+                                        isTablet, null);
+                            }
+                            recyclerView.setAdapter(eventAdapter);
+                        } else {
+                            Toast.makeText(getActivity(), R.string.msg_error_generic,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 }
