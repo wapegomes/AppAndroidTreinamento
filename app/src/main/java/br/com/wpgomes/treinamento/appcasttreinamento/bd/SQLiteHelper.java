10d9b74932003974c06dbc8a90d865ecffb3dac1
Wagner Pereira Gomes <wgomes@maccit019822.local>
2018-07-17 20:14:39 -0300

    Uso de BD - SQLite

---
+++ app/src/main/java/br/com/wpgomes/treinamento/appcasttreinamento/bd/SQLiteHelper.java
@@ -0,0 +1,38 @@
+package br.com.wpgomes.treinamento.appcasttreinamento.bd;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import br.com.wpgomes.treinamento.appcasttreinamento.util.Constants;
+
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+
+    private static final String DATABASE_NAME = "marvelapp.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public SQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(Constants.DDL_CHARACTER);
+        sqLiteDatabase.execSQL(Constants.DDL_EVENT);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Constants.CHARACTER_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Constants.EVENT_TABLE);
+        onCreate(sqLiteDatabase);
+    }
+
+    public static SQLiteDatabase getDatabase(Context context) {
+        SQLiteHelper dbHelper = new SQLiteHelper(context);
+        return dbHelper.getWritableDatabase();
+    }
+}
